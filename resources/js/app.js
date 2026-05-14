44906d9a0ef1cc76f9c6b450e99629d2dfde50cf
Gilbert Rothanglien <indominusmidas@gmail.com>
2026-05-14 17:42:34 +0530

    Fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,5 @@
 import '../css/app.css';
 import './bootstrap';
-import { registerSW } from 'virtual:pwa-register';
-
-if (import.meta.env.PROD) {
-    registerSW({ immediate: true });
-}
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
